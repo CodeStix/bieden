75643eb8a346ed22f2a137c9cbc01324627cd7d2
Stijn Rogiest <reddusted@gmail.com>
2024-12-13 21:23:47 +0100

    Flippable cards

--- src/game/scenes/GameScene.ts
+++ src/game/scenes/GameScene.ts
@@ -12,40 +12,73 @@ enum CardSuit {
     HEARTS = 2,
     DIAMONDS = 3,
 }
+const FACE_DOWN_FRAME = 13;
 
 class Card extends Phaser.GameObjects.Sprite {
+    private faceFrame: number;
+    private isFaceDown: boolean;
+    private flipTween?: Phaser.Tweens.Tween;
+
     // For value, 1 = ace ... 13 = king
     constructor(
         scene: Scene,
         x: number,
         y: number,
         public readonly suit: CardSuit,
-        public readonly value: number
+        public readonly value: number,
+        isFaceDown: boolean
     ) {
         if (value <= 0 || value > 14) {
             console.error("Invalid card value", value);
             value = 1;
         }
 
-        super(scene, x, y, "card", suit * 14 + (value - 1));
+        let fr = suit * 14 + (value - 1);
+        super(scene, x, y, "card", isFaceDown ? FACE_DOWN_FRAME : fr);
+        this.faceFrame = fr;
+
+        this.isFaceDown = isFaceDown;
 
         this.on("dragstart", () => {
-            this.scale = CARD_SCALE + 0.5;
+            // this.scale = CARD_SCALE + 0.5;
             this.depth = 100;
+            this.setFaceDown(false);
         });
         this.on("drag", (_ev: any, x: number, y: number) => {
             this.x = x;
             this.y = y;
         });
         this.on("dragend", () => {
-            this.scale = CARD_SCALE;
+            // this.scale = CARD_SCALE;
             this.depth = 1;
+            this.setFaceDown(true);
         });
     }
 
-    update(...args: any[]): void {
-        console.log("card udpateoj");
+    setFaceDown(faceDown: boolean) {
+        if (this.isFaceDown == faceDown) return;
+
+        if (this.flipTween && this.flipTween.isActive()) {
+            this.flipTween.stop();
+        }
+        this.flipTween = this.scene.tweens.add({
+            targets: this,
+            duration: 100,
+            scaleX: 0,
+            yoyo: true,
+            onYoyo: () => {
+                this.setFrame(faceDown ? FACE_DOWN_FRAME : this.faceFrame);
+            },
+            onComplete: () => {
+                this.scale = CARD_SCALE;
+            },
+        });
+        this.isFaceDown = faceDown;
     }
+}
+
+export class Player {
+    constructor(private scene: Scene) {}
 }
 
 export class GameScene extends Scene {
@@ -69,7 +102,8 @@ export class GameScene extends Scene {
                 100 + i * CARD_WIDTH * CARD_SCALE,
                 100,
                 CardSuit.DIAMONDS,
-                i + 1
+                i + 1,
+                i % 2 == 0
             )
                 .setScale(CARD_SCALE)
                 .setDepth(1)
@@ -77,6 +111,7 @@ export class GameScene extends Scene {
                     draggable: true,
                 } as Phaser.Types.Input.InputConfiguration);
             this.children.add(card);
+            // card.setFaceDown();
         }
         // this.add.sprite(30, 60, "card", 10);
 
