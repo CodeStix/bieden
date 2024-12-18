054811da8e721133fbfc68e982660531de0197c3
Stijn Rogiest <reddusted@gmail.com>
2024-12-18 17:01:04 +0100

    Create PWA

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "@radix-ui/themes/styles.css";
 import { Theme } from "@radix-ui/themes";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
     <React.StrictMode>
@@ -11,4 +12,6 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
         </Theme>
     </React.StrictMode>
 );
+
+serviceWorkerRegistration.register();
 
